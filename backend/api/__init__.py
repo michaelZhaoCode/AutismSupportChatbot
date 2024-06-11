e16987488676f855a484a5f4e50a5e10ebf4899a
michaelZhaoCode <82855036+michaelZhaoCode@users.noreply.github.com>
2024-06-11 15:38:16 -0400

    code refactoring

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,24 +1,40 @@
-# from flask import Flask
-# from .users import add_user
-# from .locations import add_location, update_location, delete_location
-# from .location_ratings import add_location_rating, update_location_rating, delete_location_rating
-# from .reviews import add_location_review, update_location_review, delete_location_review
+from flask import Flask, request, jsonify
+import cohere
+import os
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+from endpoints import populate_pdfs, add_pdf, chat
 
-# def create_app():
-#     app = Flask(__name__)
+load_dotenv("../.env")
 
-#     app.add_url_rule('/add_user', 'add_user', add_user, methods=['POST'])
-#     app.add_url_rule('/locations', 'add_location', add_location, methods=['POST'])
-#     app.add_url_rule('/locations', 'update_location', update_location, methods=['PUT'])
-#     app.add_url_rule('/locations', 'delete_location', delete_location, methods=['DELETE'])
-#     app.add_url_rule('/location_ratings', 'add_location_rating', add_location_rating, methods=['POST'])
-#     app.add_url_rule('/location_ratings', 'update_location_rating', update_location_rating, methods=['PUT'])
-#     app.add_url_rule('/location_ratings', 'delete_location_rating', delete_location_rating, methods=['DELETE'])
-#     app.add_url_rule('/location_reviews', 'add_location_review', add_location_review, methods=['POST'])
-#     app.add_url_rule('/location_reviews', 'update_location_review', update_location_review, methods=['PUT'])
-#     app.add_url_rule('/location_reviews', 'delete_location_review', delete_location_review, methods=['DELETE'])
-#     app.add_url_rule('/user_ratings', 'add_user_rating', add_user_rating, methods=['POST'])
-#     app.add_url_rule('/user_ratings', 'update_user_rating', update_user_rating, methods=['PUT'])
-#     app.add_url_rule('/user_ratings', 'delete_user_rating', delete_user_rating, methods=['DELETE'])
+api_key = os.environ["COHERE_API_KEY"]
+co = cohere.Client(api_key)
 
-#     return app
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+
+@app.route('/')
+def index():
+    return "hello"
+
+
+@app.route('/chat/', methods=['POST'])
+@cross_origin()
+def chat():
+    username = request.get_json()['username']
+    message = request.get_json()['message']
+    usertype = request.get_json()['usertype']
+
+    return jsonify({'status': 'success'}), 200
+
+
+if __name__ == "__main__":
+    # populate_pdfs('pdfs')
+    print(chat(
+        "What is autism? Could you explain the key updates and cite the relevant document titles and line numbers in "
+        "your response?",
+        'boss'), co)
--- backend/db_funcs/cluster_storage.py
+++ backend/db_funcs/cluster_storage.py
@@ -1,15 +1,25 @@
-from dotenv import load_dotenv
-load_dotenv()
-import os 
-from ..utils import *
-from collections import defaultdict
+"""
+cluster_storage.py
 
-def store_cluster(centroids, embeddings_and_names):
+This module provides functions to store, retrieve, and delete clustering data in a MongoDB database.
+"""
+from collections import defaultdict
+from ..utils import setup
+
+
+def store_cluster(centroids: list[list[float]], embeddings_and_names: list[tuple[str, list[float]]]) -> None:
+    """
+    Store the centroids and their associated embeddings in the MongoDB database.
+
+    Args:
+        centroids (list[list[float]]): A list of centroid coordinates.
+        embeddings_and_names (list[tuple[str, list[float]]]): A list of tuples, each containing a name and its
+            corresponding embedding.
+    """
     db = setup()
 
     # Select or create the embeddings collection
     clusters_collection = db['clusters']
-
 
     cluster = defaultdict(list)
     for i in range(len(centroids)):
@@ -19,16 +29,23 @@ def store_cluster(centroids, embeddings_and_names):
 
     # each embedding is tuple of (name, embedding val)
     cluster_documents = [
-    {
-    'centroid': centroid,
-    'embedding_and_name': cluster[centroid]
-    } for centroid in cluster
+        {
+            'centroid': centroid,
+            'embedding_and_name': cluster[centroid]
+        } for centroid in cluster
     ]
-   
+
     clusters_collection.insert_many(cluster_documents)
 
 
-def retrieve_cluster():
+def retrieve_cluster() -> dict[tuple[float, ...], list[tuple[str, list[float]]]]:
+    """
+    Retrieve the stored clustering data from the MongoDB database.
+
+    Returns:
+        dict[tuple[float, ...], list[tuple[str, list[float]]]]: A dictionary where the keys are centroids and the values
+            are lists of tuples containing names and embeddings.
+    """
     db = setup()
 
     clusters_collection = db['clusters']
@@ -36,7 +53,11 @@ def retrieve_cluster():
     cluster = {tuple(document['centroid']): document['embedding_and_name'] for document in clusters_collection.find()}
     return cluster
 
-def delete_cluster():
+
+def delete_cluster() -> None:
+    """
+    Delete the clustering data from the MongoDB database.
+    """
     db = setup()
     db.drop_collection('clusters')
 # delete_cluster()
