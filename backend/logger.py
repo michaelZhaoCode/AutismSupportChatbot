e39a766641b68e3b90bc3dc98d3d97562a33c401
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-10-16 19:54:44 -0400

    Added an option to initialise logger to show or hide DEBUG messages

--- backend/logger.py
+++ backend/logger.py
@@ -6,10 +6,12 @@ This module contains the logging utility functions used to set up the logger and
 import logging
 
 
-def setup_logger(filename: str) -> None:
+def setup_logger(filename: str, debug: bool = False) -> None:
     """Set up the configuration settings for the logger and return it."""
+    logger_level = logging.DEBUG if debug else logging.INFO
+
     logging.basicConfig(
-        level=logging.INFO,
+        level=logger_level,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
             logging.FileHandler(filename),
